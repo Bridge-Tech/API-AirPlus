e8a8b3be950c63d6f9b8989ff3968a0c0c62b9f9
Gouget <lucasgouget@gmail.com>
2021-09-08 21:04:34 -0300

    Mapeamento feito

---
+++ Models/TEmployee.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace api_AirPlus.Models
+{
+    [Table("t_employee")]
+    public partial class TEmployee
+    {
+        [Key]
+        [Column("id_employee")]
+        public int IdEmployee { get; set; }
+
+        [Column("nm_employee", TypeName = "varchar(100)")]
+        public string NmEmployee { get; set; }
+
+        [Column("nm_state", TypeName = "varchar(30)")]
+        public string NmState { get; set; }
+
+        [Column("nm_city", TypeName = "varchar(60)")]
+        public string NmCity { get; set; }
+
+        [Column("nm_district", TypeName = "varchar(60)")]
+        public string NmDistrict { get; set; }
+        
+        [Column("nr_phone", TypeName = "varchar(30)")]
+        public string NrPhone { get; set; }
+    }
+}
---
+++ Models/apiDbAirplusContext.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace api_AirPlus.Models
+{
+    public partial class apiDbAirplusContext : DbContext
+    {
+        public apiDbAirplusContext()
+        {
+        }
+
+        public apiDbAirplusContext(DbContextOptions<apiDbAirplusContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<TEmployee> TEmployee { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+                optionsBuilder.UseMySql("server=localhost;user id=root;password=teste123@;database=apiDbAirplus", x => x.ServerVersion("8.0.24-mysql"));
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<TEmployee>(entity =>
+            {
+                entity.HasKey(e => e.IdEmployee)
+                    .HasName("PRIMARY");
+
+                entity.Property(e => e.NmCity)
+                    .HasCharSet("utf8mb4")
+                    .HasCollation("utf8mb4_0900_ai_ci");
+
+                entity.Property(e => e.NmDistrict)
+                    .HasCharSet("utf8mb4")
+                    .HasCollation("utf8mb4_0900_ai_ci");
+
+                entity.Property(e => e.NmEmployee)
+                    .HasCharSet("utf8mb4")
+                    .HasCollation("utf8mb4_0900_ai_ci");
+
+                entity.Property(e => e.NmState)
+                    .HasCharSet("utf8mb4")
+                    .HasCollation("utf8mb4_0900_ai_ci");
+
+                entity.Property(e => e.NrPhone)
+                    .HasCharSet("utf8mb4")
+                    .HasCollation("utf8mb4_0900_ai_ci");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
